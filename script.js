3c787d87d87e0eb7b36322950226601c4199a496
Koishem <romasenkokonstantin@gmail.com>
2025-01-16 18:47:01 +0100

    modified:   index.html 	modified:   script.js 	modified:   styles.css

--- script.js
+++ script.js
@@ -1,23 +1,55 @@
 // Typewriter code
+let texts = ["Software Engineer", "Web Developer", "Creator", "Designer", "Inexperienced Idiot", "Self-taught Programmer", "Mcfunction Enthusiast"];
+let textIndex = 0;
+let typingSpeed = 40;
+let eraseSpeed = 20;
+let typewriterElement = null;
+let rolesElement = null;
 
-const text = "KOISHEM";
-const typewriterElement = document.querySelector('.typewriter');
-const typingSpeed = 40;
+function typeText() {
+    let currentText = texts[textIndex];
+    let index = 0;
 
-let index = 0;
-
-function typeText(){
-    if (index < text.length){
-        typewriterElement.textContent += text[index];
-        index++;
-        setTimeout(typeText, typingSpeed);
+    function type() {
+        if (index < currentText.length) {
+            rolesElement.textContent += currentText[index];
+            index++;
+            setTimeout(type, typingSpeed);
+        } else {
+            setTimeout(eraseText, 2000);
+        }
     }
+
+    function eraseText() {
+        if (index > 0) {
+            rolesElement.textContent = rolesElement.textContent.slice(0, index - 1);
+            index--;
+            setTimeout(eraseText, eraseSpeed);
+        } else {
+            textIndex = (textIndex + 1) % texts.length;
+            setTimeout(typeText, 1000);
+        }
+    }
+
+    type();
 }
 
 document.addEventListener("DOMContentLoaded", () => {
-    const delay = 1200;
-  
-    setTimeout(() => {
-      typeText();
-    }, delay);
-  });
+    typewriterElement = document.querySelector('.typewriter');
+    rolesElement = document.querySelector('.roles');
+    
+    let index = 0;
+    let typewriterText = "KOISHEM";
+    
+    function typeTypewriter() {
+        if (index < typewriterText.length) {
+            typewriterElement.textContent += typewriterText[index];
+            index++;
+            setTimeout(typeTypewriter, typingSpeed);
+        } else {
+            setTimeout(typeText, 1000);
+        }
+    }
+
+    setTimeout(typeTypewriter, 1400);
+});
